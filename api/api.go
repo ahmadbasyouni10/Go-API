642ea7bcd2af3d7dc3b50ed5fa9c862a474855a3
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2025-01-15 23:52:48 -0500

    Api structs

---
+++ api/api.go
@@ -0,0 +1,46 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type CoinBalanceParams struct {
+	// json username needed to let go know that the json key is username not Username
+	// go only exports fields that start with a capital letter
+	// so if we exported this struct but Username was lowercase it would not be exported
+	Username string
+}
+
+type CoinBalanceResponse struct {
+	Code    int
+	Balance int64
+}
+
+type Error struct {
+	// Error Code
+	Code int
+
+	// Error message
+	Message string
+}
+
+func writeError(w http.ResponseWriter, message string, code int) {
+	resp := Error{
+		Code:    code,
+		Message: message,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+var (
+	RequestErrorHandler = func(w http.ResponseWriter, err error) {
+		writeError(w, err.Error(), http.StatusBadRequest)
+	}
+	InternalErrorHandler = func(w http.ResponseWriter) {
+		writeError(w, "An unexpected error occured", http.StatusInternalServerError)
+	}
+)
